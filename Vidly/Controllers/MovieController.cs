91d3dc39e39c95c5ce000899895b4cedc05155df
Munawar Hassan <98495040+Munawar080@users.noreply.github.com>
2022-11-26 15:31:48 +0500

    Initial commit

---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+namespace Vidly.Controllers
+{
+	public class MovieController : Controller
+	{
+		//
+		// GET: /Movie/
+		public ViewResult Index()
+		{
+            var movie = new Movie() { Id = "MV1", Name = "Matrix" };
+
+            var customer = new List<Customer> {
+                new Customer {Name= "customer1"},
+                new Customer {Name="customer2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movies = movie,
+                Customers = customer
+            };
+
+            return View(viewModel);
+            //return Content("movie controller");
+            //return Content("hello bai sabb");
+			//return HttpNotFound();
+			//return RedirectToAction("Index", "Home", new { page = 1, sortByOrder = "ascending" });
+		}
+
+
+        // attribute routing
+        [Route("movies/released/{year:range(2010, 2020)}/{month:regex(\\d{2}):range(1,12)}")] // set the constraints to the params
+        public ActionResult ByReleaseDate(int year, int month)
+		{
+            return Content("hello freinds and fans");
+            
+		}
+		
+	}
+}
---
+++ Vidly/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class Customer
+    {
+        public int Id{ get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name{ get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+        public MembershipType MyProperty { get; set; }
+
+
+        // entity framework recognize this as a convention and treat this prop as a foreign key.
+        public byte MembershipTypeId{ get; set; }
+    }
+}
