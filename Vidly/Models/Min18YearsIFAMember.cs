b0f8cc8fa38b19055d378d8694d37d7db9d9cc48
Munawar Hassan <98495040+Munawar080@users.noreply.github.com>
2022-12-21 09:02:52 +0500

    'AddDefualtAndCustomValidationToCustomer'

---
+++ Vidly/Models/Min18YearsIFAMember.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace Vidly.Models
+{
+    public class Min18YearsIFAMember : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // cast to customer model
+            var customer = (Customer)validationContext.ObjectInstance;
+
+            if (customer.MembershipTypeId == 0 || customer.MembershipTypeId == 1) 
+                    return ValidationResult.Success;
+
+            if (customer.BirthDate == null)
+                return new ValidationResult("BirthDate is required");
+
+            // culculate the age
+            // value prop accessable if birthdate prop in model is NULLABLE
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+            return age >= 18 ? ValidationResult.Success : new ValidationResult("Minimum age should be 18 years");
+        }
+    }
+}
